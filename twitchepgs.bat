c65eeb8cea1c4acd7a1993b822907de6132a2352
engeles20 <erick.angeles@gmail.com>
2021-04-26 14:40:33 -0500

    new links

---
+++ twitchepgs.bat
@@ -0,0 +1,3 @@
+twitch-stream-check -t buddha> C:\Users\erick\Documents\GitHub\mym3ulinks\Twitchepgs.xml
+
+twitch-stream-check -t anthonyz >> C:\Users\erick\Documents\GitHub\mym3ulinks\Twitchepgs.xml
