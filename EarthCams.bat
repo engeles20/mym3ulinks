56f522b0fe83f74794c9cacd46caaf8172c1f1b0
engeles20 <erick.angeles@gmail.com>
2021-06-21 14:35:22 -0500

    new links

--- EarthCams.bat
+++ EarthCams.bat
@@ -1,86 +1,89 @@
 @echo #EXTM3U > C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 
 @echo, >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
-@echo #EXTINF:-1 group-title="Earth_Cams",Live Camera - Dallas TX - AXIS Q6215-LE >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
+@echo #EXTINF:-1 group-title="Youtube_Cams",Live Camera - Dallas TX - AXIS Q6215-LE >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 streamlink https://www.youtube.com/channel/UCdiPSngCBBo_6DBbj7oWNow/live best >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 
 @echo, >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
-@echo #EXTINF:-1 group-title="Earth_Cams",Fort Worth Stockyards Live Cam 24/7 >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
+@echo #EXTINF:-1 group-title="Youtube_Cams",Fort Worth Stockyards Live Cam 24/7 >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 streamlink https://www.youtube.com/channel/UCPOQ0aIZO6CR6vJrbLCNtGA/live best >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 
 @echo, >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
-@echo #EXTINF:-1 group-title="Earth_Cams",Galveston Texas 24/7 Live Beach Camera >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
+@echo #EXTINF:-1 group-title="Youtube_Cams",Galveston Texas 24/7 Live Beach Camera >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 streamlink https://www.youtube.com/watch?v=smDLmSZ4j80 best >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 
 @echo, >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
-@echo #EXTINF:-1 group-title="Earth_Cams",Houston Texas 24/7 Live City Camera >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
+@echo #EXTINF:-1 group-title="Youtube_Cams",Houston Texas 24/7 Live City Camera >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 streamlink https://www.youtube.com/watch?v=c5_w4yGHmNQ best >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 
 @echo, >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
-@echo #EXTINF:-1 group-title="Earth_Cams",Houston Texas 24/7 Live City Camera 2 >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
+@echo #EXTINF:-1 group-title="Youtube_Cams",Houston Texas 24/7 Live City Camera 2 >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 streamlink https://www.youtube.com/watch?v=hjqbpnigAHA best >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 
 @echo, >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
-@echo #EXTINF:-1 group-title="Earth_Cams",Times Square Duffy Square 4K View Live >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
+@echo #EXTINF:-1 group-title="Youtube_Cams",Times Square Duffy Square 4K View Live >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 streamlink https://www.youtube.com/watch?v=VYsHV7WPyFw best >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 
 @echo, >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
-@echo #EXTINF:-1 group-title="Earth_Cams",EarthCam Live Times Square in 4K >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
+@echo #EXTINF:-1 group-title="Youtube_Cams",EarthCam Live Times Square in 4K >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 streamlink https://www.youtube.com/watch?v=eJ7ZkQ5TC08 best >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 
 @echo, >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
-@echo #EXTINF:-1 group-title="Earth_Cams",Santa Monica Beach Cam powered by EXPLORE >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
+@echo #EXTINF:-1 group-title="Youtube_Cams",Santa Monica Beach Cam powered by EXPLORE >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 streamlink https://www.youtube.com/watch?v=OWbI6WtlI-k best >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 
 @echo, >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
-@echo #EXTINF:-1 group-title="Earth_Cams",Florida Live Beach Cam on Fort Myers Beach Pier at Pierside Bar and Grill >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
+@echo #EXTINF:-1 group-title="Youtube_Cams",Florida Live Beach Cam on Fort Myers Beach Pier at Pierside Bar and Grill >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 streamlink https://www.youtube.com/watch?v=l6gSenZqmeU best >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 
 @echo, >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
-@echo #EXTINF:-1 group-title="Earth_Cams",🐳 LIVE 24/7 🌴 Kaanapali Beach HD Maui Cam >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
+@echo #EXTINF:-1 group-title="Youtube_Cams",🐳 LIVE 24/7 🌴 Kaanapali Beach HD Maui Cam >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 streamlink https://www.youtube.com/channel/UCIQVWkOilfxoQSFcban_d-A/live best >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 
 @echo, >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
-@echo #EXTINF:-1 group-title="Earth_Cams",Big Bear Bald Eagle Cam >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
+@echo #EXTINF:-1 group-title="Youtube_Cams",Big Bear Bald Eagle Cam >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 streamlink https://www.youtube.com/channel/UCsFgbVuhRrPV5FqyN7kOD8g/live best >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 
 @echo, >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
-@echo #EXTINF:-1 group-title="Earth_Cams",Chicago Live Camera 360 Chicago Tilt >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
+@echo #EXTINF:-1 group-title="Youtube_Cams",Chicago Live Camera 360 Chicago Tilt >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 streamlink https://www.youtube.com/channel/UCXPSmo_oiBOHmjh_jOtTvQg/live best >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 
 @echo, >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
-@echo #EXTINF:-1 group-title="Earth_Cams",Live Cam Central Memorial Park Calgary Alberta >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
+@echo #EXTINF:-1 group-title="Youtube_Cams",Live Cam Central Memorial Park Calgary Alberta >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 streamlink https://www.youtube.com/watch?v=xsRDTfuksyI best >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 
 @echo, >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
-@echo #EXTINF:-1 group-title="Earth_Cams",Live NYC Brooklyn Bridge Manhattan cam >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
+@echo #EXTINF:-1 group-title="Youtube_Cams",Live NYC Brooklyn Bridge Manhattan cam >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 streamlink https://www.youtube.com/watch?v=KGuCGd726RA best >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 
 @echo, >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
-@echo #EXTINF:-1 group-title="Earth_Cams",Live NYC Lower Manhattan New York Harbor Cam >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
+@echo #EXTINF:-1 group-title="Youtube_Cams",Live NYC Lower Manhattan New York Harbor Cam >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 streamlink https://www.youtube.com/watch?v=Vj0XKu6AoOw best >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 
 @echo, >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
-@echo #EXTINF:-1 group-title="Earth_Cams",The University of Tampa Live Webcam >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
+@echo #EXTINF:-1 group-title="Youtube_Cams",The University of Tampa Live Webcam >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 streamlink https://www.youtube.com/watch?v=bB96qObNTSU best >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 
 @echo, >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
-@echo #EXTINF:-1 group-title="Earth_Cams",The University of Tampa Riverfront Live >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
+@echo #EXTINF:-1 group-title="Youtube_Cams",The University of Tampa Riverfront Live >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 streamlink https://www.youtube.com/watch?v=ogbkyRehtCo best >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 
 @echo, >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
-@echo #EXTINF:-1 group-title="Earth_Cams",Dania Beach Pier and Beach Cam >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
+@echo #EXTINF:-1 group-title="Youtube_Cams",Dania Beach Pier and Beach Cam >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 streamlink https://www.youtube.com/watch?v=bEoNVd3spEM best >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 
 @echo, >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
-@echo #EXTINF:-1 group-title="Earth_Cams",Las Vegas AE View Live >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
+@echo #EXTINF:-1 group-title="Youtube_Cams",Las Vegas AE View Live >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 streamlink https://www.youtube.com/watch?v=1ZNCtDLUKHw best >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 
 @echo, >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
-@echo #EXTINF:-1 group-title="Earth_Cams",Las Vegas NYNY View Live >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
+@echo #EXTINF:-1 group-title="Youtube_Cams",Las Vegas NYNY View Live >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 streamlink https://www.youtube.com/watch?v=Er_sq4DTeas best >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 
 @echo, >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
-@echo #EXTINF:-1 group-title="Earth_Cams",Hermosa Beach Good Stuff Strand Cam >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
+@echo #EXTINF:-1 group-title="Youtube_Cams",Hermosa Beach Good Stuff Strand Cam >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 streamlink https://www.youtube.com/watch?v=wc4Vy1T6hcE best >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
 
+@echo, >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
+@echo #EXTINF:-1 group-title="Earth_Cams",Wrigley Field >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
+streamlink https://www.earthcam.com/usa/illinois/chicago/wrigleyfield/?cam=wrigleyfield_hd best >> C:\Users\erick\Documents\GitHub\mym3ulinks\EarthCams.m3u
